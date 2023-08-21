557938b1944c2ec9d72551ddcbf54a3c082542c2
LRSEPT <benelghaliloris@gmail.com>
2023-08-21 14:36:27 +0100

    last version blog

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import React from "react"
 import HomePage from "./HomePage"
+import Head from "next/head"
 
 export default function Home() {
   return (
       <div className="root">
+        <Head>
+          <title>Loris Blog</title>
+          <link rel="icon" href="/favicon.ico"/>
+        </Head>
         <HomePage />
       </div>
   )
